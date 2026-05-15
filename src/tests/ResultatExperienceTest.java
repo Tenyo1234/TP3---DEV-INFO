68d095fa7033c73e0fd519037af98a23b07220e8
pcHome <labbthom@gmail.com>
2026-05-15 15:30:16 -0400

    Ajout des testes de la classe ResultatExperience

--- src/tests/ResultatExperienceTest.java
+++ src/tests/ResultatExperienceTest.java
@@ -1,7 +1,29 @@
 package tests;
 
+import logique.ResultatExperience;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResultatExperienceTest {
 
+    @Test
+    void siGetEtSetExistFonctionnent(){
+
+        ResultatExperience r1 = new ResultatExperience();
+
+        r1.setExiste(true);
+
+        assertTrue(r1.getExiste());
+    }
+
+    @Test
+    void siGetEtSetSuccessFonctionnent(){
+
+        ResultatExperience r1 = new ResultatExperience();
+
+        r1.setSuccess(true);
+
+        assertTrue(r1.getSuccess());
+    }
 }
